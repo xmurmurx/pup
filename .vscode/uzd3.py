ea762ce114225501790eff6c86096ad9f7f4fb60
xmurmurx <xmurmurx@users.noreply.github.com>
2021-02-23 12:48:45 +0200

    uzd 3

---
+++ .vscode/uzd3.py
@@ -0,0 +1,25 @@
+def materialuAprekins(garums, platums, augstums, skaits):
+    print(garums*garums)
+    materialaUzskaite("STURIS", garums,augstums,skaits)
+
+def materialaUzskaite(tips, izmars1, izmers2, skaits):
+     print(tips)
+if (tips == "FINERIS"):
+     print("skaits-", skaits):
+     print("izmers1-",izmers1)
+     print("izmers2-",izmers2)
+#tad   
+     
+
+elif (tips == "LISTE"):
+        print("Skaits-",skaits)
+        print("izmers1-",izmers1)
+#tad
+
+elif (tips == "STURIS"):
+ print("Skaits-",skaits):          
+     #tad
+
+
+
+materialuAprekins (2,3,4,5)
